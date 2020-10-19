6645387cbfd0ffe6b3f0f7546d3c1a4cd1ba254a
chn19cs053 <72916722+chn19cs053@users.noreply.github.com>
2020-10-19 10:03:14 +0530

    Create FizzBuzz.js
    Addition of the  FizzBuzz  code to the repo.

---
+++ JavaScript/FizzBuzz.js
@@ -0,0 +1,19 @@
+  
+var n=prompt("Enter the number of terms to be generated :");
+function fizzbuzz(){
+  for(var count=1;count<= n;count++){
+    if(count %3===0 &&count % 5===0){
+      output.push("FizzBuzz");
+    }
+    else if(count %3===0){
+      output.push("Fizz");
+    }
+    else if(count%5===0){
+      output.push("Buzz");
+    }
+    else{
+      output.push(count);
+    }
+    console.log(output);
+}
+  
